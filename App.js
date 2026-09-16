e04a1a8ba7ce799eb44f645f647a5844a1f44410
Davide Sangalli <da.sangalli@gmail.com>
2026-09-16 08:11:48 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import { StyleSheet, SafeAreaView, StatusBar, Linking } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const handleShouldStartLoad = (event) => {
+    const { url } = event;
+    
+    // Permette solo i normali link web HTTP e HTTPS
+    if (url.startsWith('http://') || url.startsWith('https://')) {
+      return true;
+    }
+    
+    // Se il sito prova ad aprire app esterne (store, social, ecc.), prova a gestirle o le blocca
+    try {
+      Linking.canOpenURL(url).then((supported) => {
+        if (supported) {
+          Linking.openURL(url);
+        }
+      });
+    } catch (e) {
+      console.log('Impossibile aprire il link:', e);
+    }
+    
+    // Impedisce alla WebView di caricare schemi sconosciuti facendo fallire la pagina
+    return false;
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#121212" />
@@ -13,7 +36,8 @@ export default function App() {
         domStorageEnabled={true}
         allowsInlineMediaPlayback={true}
         mediaPlaybackRequiresUserAction={false}
-        originWhitelist={['*']}
+        originWhitelist={['http://*', 'https://*']}
+        onShouldStartLoadWithRequest={handleShouldStartLoad}
       />
     </SafeAreaView>
   );
